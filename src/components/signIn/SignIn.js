a2bbc0884d3e42780284d6d28014e1db31d10608
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-02 02:00:10 +1200

    Finished Login page for both instructors and students

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -10,23 +10,22 @@ class SignIn extends React.Component {
     });
   }
 
-  changeLogin(){
+  toInstructor = () => {
     this.setState({
       loginState: "instructor"
     })
   }
 
-  toggleLogin(){
-    if (this.state.loginState === "student"){
-
-    } else {
-
-    }
+  toStudent = () => {
+    this.setState({
+      loginState: "student"
+    })
   }
 
-  render() {
-    return (
-      <form>
+  toggleLogin(){
+    if (this.state.loginState === "student"){
+      return(
+        <form className = "sign-in-form">
         <h3>Sign In as a <strong>Student</strong></h3>
         <div className = "form-group">
           <label>Email Address</label>
@@ -49,8 +48,51 @@ class SignIn extends React.Component {
         <div style = {{textAlign: "center"}}>
           Forgot <a href="#">password?</a>
         </div>
+        <div>
+            Are you an <a onClick ={this.toInstructor} href="javascript:;">instructor</a>?
+          </div>
       </form>
-    );
+      )
+    } else {
+      return (
+        <form className = "sign-in-form">
+          <h3>Sign In as an <strong>Instructor</strong></h3>
+          <div className = "form-group">
+            <label>Email Address</label>
+            <input type = "email" className = "form-control" placeholder= "Enter Email" style = {{width: "50%", margin: "auto"}}/>
+          </div>
+  
+          <div className = "form-group">
+            <label>Password</label>
+            <input type = "password" className = "form-control" placeholder = "Enter Password" style = {{width: "50%", margin: "auto"}}/>
+          </div>
+  
+          <div className = "form-group">
+            <div className = "custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="customCheck1" />
+              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            </div>  
+          </div>
+  
+          <button type="submit" className="btn btn-primary btn-block" style = {{width: "50%", margin: "auto"}}>Submit</button>
+          <div style = {{textAlign: "center"}}>
+            Forgot <a href="#">password?</a>
+          </div>
+          <div>
+            Are you a <a onClick ={this.toStudent} href="javascript:;">student</a>?
+          </div>
+        </form>
+      );
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        {this.toggleLogin()}
+      </div>
+    )
+    
   }
 }
 
