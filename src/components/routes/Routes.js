6e6e3be81614973242265f644b00c9703c6c3c79
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-02 03:35:41 +1200

    Reverting changes of HashRouter and Routes.js

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -8,17 +8,11 @@ import SignIn from '../signIn/SignIn';
 class Routes extends React.Component {
     render() {
         return (
-            // <Switch>
-            //     <Route path="/" component={ LandingPage } exact/>
-            //     <Route path="/explore" component={ Explore } exact/>
-            //     <Route path="/instructors" component={ Instructors } exact/>
-            //     <Route path="/signin" component={ SignIn } exact/>
-            // </Switch>
             <Switch>
-                <Link to = "learnFromAKiwi/" component = { LandingPage } />
-                <Link to = "learnFromAKiwi/explore" component = { Explore } />
-                <Link to = "learnFromAKiwi/instructors" component = { Instructors } />
-                <Link to = "learnFromAKiwi/signin" component = { SignIn } />
+                <Route path="/" component={ LandingPage } exact/>
+                <Route path="/explore" component={ Explore } exact/>
+                <Route path="/instructors" component={ Instructors } exact/>
+                <Route path="/signin" component={ SignIn } exact/>
             </Switch>
         );
     }
