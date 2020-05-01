3df6247ade99cb61efad9c3dd0267b9ebceb548e
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-02 03:19:53 +1200

    Reverted Link changes in Routes.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <HashRouter basename = {"/learnFromAKiwi"}>
+  <BrowserRouter basename = {"/learnFromAKiwi"}>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
   // added learnfromAKiwi basename
 );
