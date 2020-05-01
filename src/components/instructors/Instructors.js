12c1151dc5b71f83c12b4c8ac794e4ca51fc8939
Sunny Feng <sunnyfengnz@msn.com>
2020-05-02 00:58:45 +1200

    changed navbar links

---
+++ src/components/instructors/Instructors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Instructors extends React.Component {
+  render() {
+    return (
+        <h1>Instructors</h1>
+    );
+  }
+}
+
+export default Instructors;
--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from '../landingPage/LandingPage';
-import Login from '../login/Login';
+import Explore from '../explore/Explore';
+import Instructors from '../instructors/Instructors';
+import SignIn from '../signIn/SignIn';
 
 class Routes extends React.Component {
     render() {
         return (
             <Switch>
                 <Route path="/" component={ LandingPage } exact/>
-                <Route path="/login" component={ Login } exact/>
+                <Route path="/explore" component={ Explore } exact/>
+                <Route path="/instructors" component={ Instructors } exact/>
+                <Route path="/signin" component={ SignIn } exact/>
             </Switch>
         );
     }
