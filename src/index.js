01fec9e1525505bec34aaf2d87935ca533ce20c6
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-03 13:03:12 +1200

    Implemented the carousel feature on landing page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   document.getElementById('root')
-  // added learnfromAKiwi basename
 );
 
 // If you want your app to work offline and load faster, you can change
