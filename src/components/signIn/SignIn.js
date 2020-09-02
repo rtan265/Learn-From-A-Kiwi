2eb997e72f5cb5b208c75ab61832a2f9b99b6a71
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-09-02 23:29:12 +1200

    Code to deploy learn from a kiwi

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './SignIn.css'; 
 
-import logo from '../../images/logo-signin.png';
-
 class SignIn extends React.Component {
   constructor(props){
     super(props);
@@ -28,8 +26,7 @@ class SignIn extends React.Component {
     if (this.state.loginState === "student"){
       return(
         <form className = "sign-in-form">
-        <img src={logo} />
-        <h3 className="title-signin">Sign In as a <strong>Student</strong></h3>
+        <h3>Sign In as a <strong>Student</strong></h3>
         <div className = "form-group">
           <label>Email Address</label>
           <input type = "email" className = "form-control" placeholder= "Enter Email" style = {{width: "50%", margin: "auto"}}/>
@@ -59,8 +56,7 @@ class SignIn extends React.Component {
     } else {
       return (
         <form className = "sign-in-form">
-          <img src={logo} />
-          <h3 className="title-signin">Sign In as an <strong>Instructor</strong></h3>
+          <h3>Sign In as an <strong>Instructor</strong></h3>
           <div className = "form-group">
             <label>Email Address</label>
             <input type = "email" className = "form-control" placeholder= "Enter Email" style = {{width: "50%", margin: "auto"}}/>
